a0c14a0246c1d1be74d830ceccc63d5098248dc3
marie-bisbo <marie@brainlabsdigital.com>
2022-01-04 09:05:33 +0000

    Setup basic attack

--- RPG Project/Assets/Scripts/PlayerController.cs
+++ RPG Project/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using RPG.Movement;
+﻿using RPG.Combat;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control
@@ -6,6 +7,27 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -15,13 +37,17 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
